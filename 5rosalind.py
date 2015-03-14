d1b2d8800d5be0500ac657c37d0527e6d614444e
Gabriel Redig <glredig@gmail.com>
2015-03-13 20:46:29 -0700

    Working solution to challenge 5

--- 5rosalind.py
+++ 5rosalind.py
@@ -8,28 +8,34 @@ def max_gc_content():
 	str = ''
 	with open(filename, "r") as openfile:
 		for line in openfile:
-			if re.match(r'>*\w', line):
+			if re.match(r'>(\w){13}', line):
 				""" If the previous id had bigger gc_count, set max """
-				if str and gc_count(str) > max_count:
-					max_id = id
-					max_count = gc_count(str)
+				if str:
+					if gc_count(str) > max_count:
+						max_id = id
+						max_count = gc_count(str)
 						
-				matchObj = re.match(r'\w', line)
-				id = matchObj.group()
+				matchObj = re.match(r'>(\w){13}', line)
+				id = matchObj.group(0)
 				str = ''
 			else:
-				str += line		
-	print id, max_count
+				matchObj = re.match(r'[^\s]*', line)
+				str += matchObj.group(0)
+
+		if gc_count(str) > max_count:
+			max_id = id
+			max_count = gc_count(str)		
+	print max_id[1:]
+	print  max_count * 100.0
 	
 
-def gc_content(str):
+def gc_count(str):
         dna = str
-        g_or_c = 0
+        g_or_c = 0.0
         for i in dna:
                 if i == "G" or i == "C":
                         g_or_c += 1
-
-        print g_or_c / len(str)
-        return g_or_c / len(str)
+	#print g_or_c / float(len(dna))
+	return g_or_c / float(len(dna))
 
 max_gc_content()
