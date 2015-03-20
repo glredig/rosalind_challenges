b39bd2a8674fb793ecda93c4b8253ddb1d92907a
Gabriel Redig <glredig@gmail.com>
2015-03-19 17:25:10 -0700

    Challenge 8 rewrite

--- 8rosalind.py
+++ 8rosalind.py
@@ -1,62 +1,36 @@
-import re
-
-def common_ancestor():
+def consensus():
 	filename = raw_input("File: ")
+	consensus = ''
 	strands = []
 	strand = ''
 	counts = []
-	final_string = ''
-	a, t, g, c, = [], [], [], []
-	with open(filename, "r") as openFile:
-		for line in openFile:
-			if re.match(r'>(\w*)', line):
+
+	with open(filename, "r") as f:
+		for line in f.read():
+			if line.startswith(">"):
 				if strand:
-					strands.append(strand.rstrip())
+					strands.append(strand)				
 				strand = ''
 			else:
-				strand += line
-		strands.append(strand.rstrip())
-	for i in range(len(strands[0])):
-		a_0, t_0, g_0, c_0 = 0, 0, 0, 0
+				strand += line.rstrip()
 
-		for j in range(len(strands)):
-			if strands[j][i] == "A":
-				a_0 += 1
-			elif strands[j][i] == "T":
-				t_0 += 1
-			elif strands[j][i] == "G":
-				g_0 += 1
-			elif strands[j][i] == "C":
-				c_0 += 1
+	for nt in range(len(strands[0] - 1):
+		max_num = 0
+		max_ltr = ''
+		a, c, t, g = 0, 0, 0, 0
 
-		count = {'A': a_0, 'T': t_0, 'G': g_0, 'C': c_0}
-		counts.append(count)
-	
-	print counts
-	for item in counts:
-		max = ''
-		max_val = 0
-		for key in item:
-			if key == 'A':
-				max_val = item[key]
-				max = 'A'
-				a.append(item[key])
-			else:
-				if item[key] > max_val:
-					max_val = item[key]
-					max = key
-				if key == 'T':
-					t.append(item[key])
-				elif key == 'G':
-					g.append(item[key])
-				elif key == 'C':
-					c.append(item[key])
-		final_string += max
-	
-	print final_string
-	print "A: %s" % ' '.join(map(str, a))
-	print "C: %s" % ' '.join(map(str, c))
-	print "G: %s" % ' '.join(map(str, g))
-	print "T: %s" % ' '.join(map(str, t))
-	
-common_ancestor()
+		for seq in strands:
+			if strands[seq][nt] == 'A':
+				a += 1
+			elif strands[seq][nt] == 'T':
+				t += 1
+			elif strands[seq][nt] == 'G':
+				g += 1
+			elif strands[seq][nt] == 'C':
+				c += 1
+		
+
+
+
+
+consensus():
