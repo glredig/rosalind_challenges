8ae2a21f2faadbabd892c38099752bd0d4d86271
Gabriel Redig <glredig@gmail.com>
2015-03-24 18:01:14 -0700

    Challenge 15 - shared motif (passing example)

--- 15rosalind.py
+++ 15rosalind.py
@@ -7,14 +7,27 @@ def shared_motif():
 	current_string = ''
 
 	pairs = fasta.read_fasta_from_file(filename)
-	max_string, current_string = pairs[0][1]
+	current_string = pairs[0][1][1]
+
+	def find_max_motif(motif):
+		common = True
+		for j in xrange(1, len(pairs) - 1):
+			if motif in pairs[j][1]:
+				print motif
+			else:
+				common = False
+		return common
+
 	for i in xrange(len(pairs[0][1]) - 1):
-		while find_max_motif(current_string):
-			if len(current_string) > max_string:
+		diff = 1
+		while find_max_motif(current_string) and (i + diff < len(pairs[0][1])):
+			if len(current_string) > len(max_string):
 				max_string = current_string
 			
-			if len(pairs[0][1]) > (i + 1):
-				current_string += pairs[0][1][i + 1]
+			if len(pairs[0][1]) > (i + diff):
+				current_string += pairs[0][1][i + diff]
+				print "added to string"
+			diff += 1
 		else:
 			 current_string = ''
 
@@ -22,14 +35,6 @@ def shared_motif():
 		print current_string	
 		print pairs[0][1][i]
 
-find_max_motif(motif):
-	common = True
-	for j in xrange(1, len(pairs) - 1):
-		if motif in pairs[j][1]:
-			print motif
-		else:
-			common = False
-	return common
-
+	print "Max string: %s" % max_string
 
 shared_motif()
